38327a531399954c18e5e1973eef752ae370dcb5
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-02 01:26:24 +0300

    img

--- Expo 2030/src/Pages/SignIn.jsx
+++ Expo 2030/src/Pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import r from '../assets/r.jpg'
 
 function SignIn() {
 
@@ -34,54 +35,50 @@ function SignIn() {
 
   return (
     <>
-        <div className='max-sm:grid-cols-1 max-md:grid-cols-1 md:grid-cols-1 h-screen w-full grid lg:grid-cols-2'>
-            <div className='bg-no-repeat bg-cover max-sm:h-[50vh] md:h-[50vh] lg:h-full' style={{backgroundImage: `url('https://e0.pxfuel.com/wallpapers/1011/158/desktop-wallpaper-kingdom-tower-saudi-arabia-towers-riyadh.jpg')`}}>
-
-            </div>
+        {/* <div className='h-screen w-full'> */}
+            <div className='bg-no-repeat bg-cover h-screen flex flex-col justify-center items-center' style={{backgroundImage: `url(${r})`}}>
 
             <div>
 
-            {/* sign in  */}
-            <div className='flex flex-col lg:justify-center items-center h-full'>
-            <div className='bg-white flex flex-col p-10 w-96'>
-            <p className='text-lg text-center'>Sign In Now</p>
+                {/* sign in  */}
 
-            <input
-            className='border rounded-lg mt-5'
-            type='text'
-            name='Name'
-            placeholder='Name'
-            value={name}
-            onChange={(event)=>{setName(event.target.value)}}
-            />
+                <div className='bg-[rgba(255,255,255,0.46)] flex flex-col p-10 w-96 items-center justify-center rounded-lg'>
+                <p className='text-lg text-center'>Sign In Now</p>
 
-            <input
-            className='border rounded-lg mt-5'
-            type='password'
-            name='password'
-            placeholder='password'
-            value={password}
-            onChange={(event)=>{setPassword(event.target.value)}}
-            />
+                <input
+                className='border rounded-lg mt-5'
+                type='text'
+                name='Name'
+                placeholder='Name'
+                value={name}
+                onChange={(event)=>{setName(event.target.value)}}
+                />
 
-            <p className='mt-5'>You don't have an account? <Link className='font-bold' to='/SignUp'> make one</Link></p>
+                <input
+                className='border rounded-lg mt-5'
+                type='password'
+                name='password'
+                placeholder='password'
+                value={password}
+                onChange={(event)=>{setPassword(event.target.value)}}
+                />
 
-          </div>
+                <p className='mt-5'>You don't have an account? <Link className='font-bold' to='/SignUp'> make one</Link></p>
+                {validationError && (
+                <p className='text-red-500'>{validationError}</p>
+                )}
 
-          {validationError && (
-            <p className='text-red-500'>{validationError}</p>
-          )}
-
-            <button
-            className="text-white mt-5 bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center transition-transform transform-gpu hover:scale-105 dark:bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-5"
-            type="button" onClick={sign_In}
-            >
-            Sign In
-            </button>
-
-            </div>
+                <button
+                className="text-white mt-5 bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center transition-transform transform-gpu hover:scale-105 dark:bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-5"
+                type="button" onClick={sign_In}
+                >
+                Sign In
+                </button>
+                </div>
+             
+            </div>     
         </div>
-            </div>
+            {/* </div> */}
 
     </>
   )
