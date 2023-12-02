619b2bb782610c6cd84b16c8e4be73eb540f1806
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-03 00:29:43 +0300

    error page

--- Expo 2030/src/Errors/Error.jsx
+++ Expo 2030/src/Errors/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import error from '../assets/error.svg'
+
 
 function Error() {
   return (
-    <div>Error</div>
+    <>
+      <div>
+        <img className='h-screen w-screen' src={error} alt="" />
+      </div>
+    </>
   )
 }
 
